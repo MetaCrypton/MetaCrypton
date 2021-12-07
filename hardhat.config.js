cf59e900a2ffc88b7d121bc7a95fb238fdb67eed
BaldyAsh <antongrigorjev2010@gmail.com>
2021-12-08 02:01:32 +0400

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,4 +18,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
+  mocha: {
+    timeout: 20000
+  }
 };
