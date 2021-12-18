64bc045e78657d1f3b389b9ea6c8b417b4db4e9f
BaldyAsh <antongrigorjev2010@gmail.com>
2021-12-18 23:52:17 +0400

    Started testing upgrades registry

---
+++ test/upgrades-registry-test.js
@@ -0,0 +1,53 @@
+const { expect, assert } = require("chai");
+const { ethers, waffle } = require("hardhat");
+const { keccak256 } = require('@ethersproject/solidity');
+
+describe("Upgrades registry", function() {
+    this.timeout(20000)
+
+    let admin, alice, bob, charlie;
+    let coder;
+
+    let upgradesRegistry;
+    let upgradesRegistryUpgrade;
+    let upgradesRegistryUpgradable;
+
+    let test2;
+    let test;
+    let testUpgrade;
+    let testUpgradable;
+
+    async function deploy(contractName, ...args) {
+        const Factory = await ethers.getContractFactory(contractName, admin)
+        const instance = await Factory.deploy(...args)
+        return instance.deployed()
+    }
+
+    it("Deploy Upgrades Registry", async function() {
+        [admin, alice, bob, charlie] = await ethers.getSigners();
+        coder = ethers.utils.defaultAbiCoder;
+
+        const upgradesRegistryInit = await deploy("UpgradesRegistryInit");
+        const upgradesRegistryProxy = await deploy("UpgradesRegistryProxy", upgradesRegistryInit.address);
+
+        const upgradesRegistryInitializable = await ethers.getContractAt("IInitializable", upgradesRegistryProxy.address);
+        upgradesRegistryUpgrade = await ethers.getContractAt("IUpgrade", upgradesRegistryProxy.address);
+        upgradesRegistryUpgradable = await ethers.getContractAt("IUpgradable", upgradesRegistryProxy.address);
+        upgradesRegistry = await ethers.getContractAt("IUpgradesRegistry", upgradesRegistryProxy.address);
+
+        await upgradesRegistryInitializable.initialize(coder.encode(["address"], [admin.address]));
+    });
+
+    it("Deploy test proxy and upgrades", async function() {
+        const test1 = await deploy("Test1");
+        test2 = await deploy("Test2");
+        testProxy = await deploy("TestProxy", test1.address);
+
+        const testInitializable = await ethers.getContractAt("IInitializable", testProxy.address);
+        testUpgrade = await ethers.getContractAt("IUpgrade", testProxy.address);
+        testUpgradable = await ethers.getContractAt("IUpgradable", testProxy.address);
+        test = await ethers.getContractAt("ITest", testProxy.address);
+
+        await testInitializable.initialize(coder.encode(["address", "address"], [admin.address, upgradesRegistry.address]));
+    });
+});
